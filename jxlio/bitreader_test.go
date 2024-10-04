6a6055265ee2bafd976cd329cb993cbbcb8c3f7a
Ken Faulkner <Ken.Faulkner@gmail.com>
2024-10-04 22:27:19 +1000

    add TestReadbits

--- jxlio/bitreader_test.go
+++ jxlio/bitreader_test.go
@@ -15,13 +15,13 @@ func TestReadbit(t *testing.T) {
 		expectErr bool
 	}{
 		{
-			name:      "little endian 1",
+			name:      "result 1",
 			data:      []uint8{0x01},
 			expected:  1,
 			expectErr: false,
 		},
 		{
-			name:      "little endian 0",
+			name:      "result 0",
 			data:      []uint8{0xFE},
 			expected:  0,
 			expectErr: false,
@@ -48,63 +48,69 @@ func TestReadbit(t *testing.T) {
 	}
 }
 
-// TestReadbits tests the reading of multiple bits
-//func TestReadbits(t *testing.T) {
-//
-//	for _, tc := range []struct {
-//		name         string
-//		data         []uint8
-//		littleEndian bool
-//		expected     uint8
-//		expectErr    bool
-//	}{
-//		{
-//			name:         "little endian 0",
-//			data:         []uint8{0x01, 0xFF},
-//			littleEndian: true,
-//			expected:     0,
-//			expectErr:    false,
-//		},
-//		{
-//			name:         "little endian 1",
-//			data:         []uint8{0xFF, 0x01},
-//			littleEndian: true,
-//			expected:     1,
-//			expectErr:    false,
-//		},
-//		{
-//			name:         "big endian 0",
-//			data:         []uint8{0x01, 0xFF},
-//			littleEndian: true,
-//			expected:     0,
-//			expectErr:    false,
-//		},
-//		{
-//			name:         "big endian 1",
-//			data:         []uint8{0xFF, 0x01},
-//			littleEndian: true,
-//			expected:     1,
-//			expectErr:    false,
-//		},
-//	} {
-//		t.Run(tc.name, func(t *testing.T) {
-//
-//			data := bytes.NewReader(tc.data)
-//			br := NewBitreader(data, false)
-//
-//			resp, err := br.readBit()
-//			if err != nil && !tc.expectErr {
-//				t.Errorf("got error when none was expected : %v", err)
-//			}
-//
-//			if err == nil && tc.expectErr {
-//				t.Errorf("expected error but got none")
-//			}
-//
-//			if resp != tc.expected {
-//				t.Errorf("expected %v but got %v", tc.expected, resp)
-//			}
-//		})
-//	}
-//
-//}
+// TestReadbits tests the reading multiple bits.
+func TestReadbits(t *testing.T) {
+
+	for _, tc := range []struct {
+		name      string
+		data      []uint8
+		numBits   uint32
+		expected  uint64
+		expectErr bool
+	}{
+		{
+			name:      "4 bits",
+			data:      []uint8{0x0F},
+			numBits:   4,
+			expected:  15,
+			expectErr: false,
+		},
+		{
+			name:      "7 bits",
+			data:      []uint8{0xFF},
+			numBits:   7,
+			expected:  127,
+			expectErr: false,
+		},
+		{
+			name:      "8 bits",
+			data:      []uint8{0xFF},
+			numBits:   8,
+			expected:  255,
+			expectErr: false,
+		},
+		{
+			name:      "10 bits, expecting b1011111111",
+			data:      []uint8{0xFF, 0x02},
+			numBits:   10,
+			expected:  0x02FF,
+			expectErr: false,
+		},
+		{
+			name:      "32 bits",
+			data:      []uint8{0xFF, 0x02, 0x03, 0xD4},
+			numBits:   32,
+			expected:  0xD40302FF,
+			expectErr: false,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+
+			data := bytes.NewReader(tc.data)
+			br := NewBitreader(data)
+
+			resp, err := br.ReadBits(tc.numBits)
+			if err != nil && !tc.expectErr {
+				t.Errorf("got error when none was expected : %v", err)
+			}
+
+			if err == nil && tc.expectErr {
+				t.Errorf("expected error but got none")
+			}
+
+			if resp != tc.expected {
+				t.Errorf("expected %v but got %v", tc.expected, resp)
+			}
+		})
+	}
+}
