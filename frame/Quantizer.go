6c79fe1ea648538c2c29a5a080387181b6d82807
Ken Faulkner <Ken.Faulkner@gmail.com>
2024-10-06 15:28:07 +1100

    adaptiveSmooth implemented

--- frame/Quantizer.go
+++ frame/Quantizer.go
@@ -1,4 +1,27 @@
 package frame
 
+import "github.com/kpfaulkner/jxl-go/jxlio"
+
 type Quantizer struct {
+	globalScale   uint32
+	quantLF       uint32
+	scaledDequant []float32
+}
+
+func NewQuantizerWithReader(reader *jxlio.Bitreader, lfDequant []float32) (*Quantizer, error) {
+	q := &Quantizer{}
+	var err error
+	q.globalScale, err = reader.ReadU32(1, 11, 2049, 11, 4097, 12, 8193, 16)
+	if err != nil {
+		return nil, err
+	}
+	q.quantLF, err = reader.ReadU32(16, 0, 1, 5, 1, 8, 1, 16)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 0; i < 3; i++ {
+		q.scaledDequant[i] = (1 << 16) * lfDequant[i] / float32(q.globalScale*q.quantLF)
+	}
+	return q, nil
 }
--- frame/lfcoefficients.go
+++ frame/lfcoefficients.go
@@ -2,6 +2,7 @@ package frame
 
 import (
 	"errors"
+	"math"
 
 	"github.com/kpfaulkner/jxl-go/jxlio"
 	"github.com/kpfaulkner/jxl-go/util"
@@ -20,7 +21,7 @@ func NewLFCoefficientsWithReader(reader *jxlio.Bitreader, parent LFGroup, frame 
 	lf.lfIndex = util.MakeMatrix2D[int32](parent.size.Height, parent.size.Width)
 	header := frame.Header
 	adapativeSmoothing := (header.Flags & (SKIP_ADAPTIVE_LF_SMOOTHING | USE_LF_FRAME)) == 0
-	info := make([]*ModularChannel, 3)
+	info := make([]ModularChannel, 3)
 	dequantLFCoeff := util.MakeMatrix3D[float32](3, 0, 0)
 	subSampled := header.jpegUpsamplingY[0] != 0 || header.jpegUpsamplingY[1] != 0 || header.jpegUpsamplingY[2] != 0 ||
 		header.jpegUpsamplingX[0] != 0 || header.jpegUpsamplingX[1] != 0 || header.jpegUpsamplingX[2] != 0
@@ -30,7 +31,7 @@ func NewLFCoefficientsWithReader(reader *jxlio.Bitreader, parent LFGroup, frame 
 	for i := 0; i < 3; i++ {
 		sizeY := parent.size.Height >> header.jpegUpsamplingY[i]
 		sizeX := parent.size.Width >> header.jpegUpsamplingX[i]
-		info[cMap[i]] = NewModularChannelWithAllParams(sizeY, sizeX, header.jpegUpsamplingY[i], header.jpegUpsamplingX[i], false)
+		info[cMap[i]] = *NewModularChannelWithAllParams(sizeY, sizeX, header.jpegUpsamplingY[i], header.jpegUpsamplingX[i], false)
 		dequantLFCoeff[i] = util.MakeMatrix2D[float32, uint32](sizeY, sizeX)
 	}
 
@@ -48,7 +49,136 @@ func NewLFCoefficientsWithReader(reader *jxlio.Bitreader, parent LFGroup, frame 
 		}
 	}
 
-	panic("not implemented yet")
+	extraPrecision, err := reader.ReadBits(2)
+	if err != nil {
+		return nil, err
+	}
+	lfQuantStream, err := NewModularStreamWithStreamIndex(reader, frame, int(1+parent.lfGroupID), info)
+	if err != nil {
+		return nil, err
+	}
 
+	err = lfQuantStream.decodeChannels(reader, false)
+	if err != nil {
+		return nil, err
+	}
+	lfQuant := lfQuantStream.getDecodedBuffer()
+	scaledDequant := frame.LfGlobal.quantizer.scaledDequant
+	for i := 0; i < 3; i++ {
+		c := cMap[i]
+		sd := scaledDequant[i] / (1 << extraPrecision)
+		for y := 0; y < len(lfQuant[c]); y++ {
+			dq := dequantLFCoeff[i][y]
+			q := lfQuant[c][y]
+			for x := 0; x < len(lfQuant[c][y]); x++ {
+				dq[x] = float32(q[x]) * sd
+			}
+		}
+	}
+
+	if !subSampled {
+
+		// TOOD(kpfaulkner) investigate what this really does.
+		lfc := frame.LfGlobal.lfChanCorr
+		kX := lfc.baseCorrelationX + float32(lfc.xFactorLF-128)/float32(lfc.colorFactor)
+		kB := lfc.baseCorrelationB + float32(lfc.bFactorLF-128)/float32(lfc.colorFactor)
+		dqLFY := dequantLFCoeff[1]
+		dqLFX := dequantLFCoeff[0]
+		dqLFB := dequantLFCoeff[2]
+		for y := 0; y < len(dqLFY); y++ {
+			dqLFYy := dqLFY[y]
+			dqLFXy := dqLFX[y]
+			dqLFBY := dqLFB[y]
+			for x := 0; x < len(dqLFYy); x++ {
+				dqLFXy[x] += kX * dqLFYy[x]
+				dqLFBY[x] += kB * dqLFYy[x]
+			}
+		}
+	}
+
+	if adapativeSmoothing {
+		lf.dequantLFCoeff = adaptiveSmooth(dequantLFCoeff, scaledDequant)
+	} else {
+		lf.dequantLFCoeff = dequantLFCoeff
+	}
+
+	err = lf.populatedLFIndex(parent, lfQuant)
 	return lf, nil
+}
+
+func (c LFCoefficients) populatedLFIndex(parent LFGroup, quant [][][]int32) error {
+	panic("not implemented yet")
+	return nil
+}
+
+func adaptiveSmooth(coeff [][][]float32, scaledDequant []float32) [][][]float32 {
+	weighted := make([][][]float32, 3)
+	gap := make([][]float32, len(coeff[0]))
+	dequantLFCoeff := make([][][]float32, 3)
+	for i := 0; i < 3; i++ {
+		co := coeff[i]
+		weighted[i] = make([][]float32, len(co))
+		sd := scaledDequant[i]
+		for y := 01; y < len(co)-1; y++ {
+			coy := co[y]
+			coym := co[y-1]
+			coyp := co[y+1]
+			if gap[y] == nil {
+				gap[y] = make([]float32, len(coy))
+				for x := 0; x < len(gap[y]); x++ {
+					gap[y][x] = 0.5
+				}
+			}
+			gy := gap[y]
+			weighted[i][y] = make([]float32, len(coy))
+			wy := weighted[i][y]
+			for x := 01; x < len(coy)-1; x++ {
+				sample := coy[x]
+				adjacent := coy[x-1] + coy[x+1] + coym[x] + coyp[x]
+				diag := coym[x-1] + coym[x+1] + coyp[x-1] + coyp[x+1]
+				wy[x] = 0.05226273532324128*sample + 0.20345139757231578*adjacent + 0.0334829185968739*diag
+				g := float32(math.Abs(float64(sample-wy[x])) * float64(sd))
+				if g > gy[x] {
+					gy[x] = g
+				}
+			}
+		}
+	}
+
+	for y := 0; y < len(gap); y++ {
+		if gap[y] == nil {
+			continue
+		}
+		gy := gap[y]
+		for x := 0; x < len(gy); x++ {
+			gy[x] = util.Max[float32](0.0, 3.0-4.0*gy[x])
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		co := coeff[i]
+		dequantLFCoeff[i] = make([][]float32, len(co))
+		dqi := dequantLFCoeff[i]
+		wi := weighted[i]
+		for y := 0; y < len(co); y++ {
+			coy := co[y]
+			dqi[y] = make([]float32, len(coy))
+			dqy := dqi[y]
+			gy := gap[y]
+			wiy := wi[y]
+			if y == 0 || y+1 == len(co) {
+				copy(dqy, coy)
+				continue
+			}
+			for x := 0; x < len(coy); x++ {
+				if x == 0 || x+1 == len(coy) {
+					dqy[x] = coy[x]
+					continue
+				}
+				dqy[x] = (coy[x]-wiy[x])*gy[x] + wiy[x]
+			}
+
+		}
+	}
+	return dequantLFCoeff
 }
