4fe9306f340c975ccd378594c57bcb58f0bcab44
Ken Faulkner <Ken.Faulkner@gmail.com>
2024-10-19 18:12:56 +1100

    implementing invertVarDCT

--- frame/PassGroup.go
+++ frame/PassGroup.go
@@ -49,7 +49,7 @@ func NewPassGroupWithReader(reader *jxlio.Bitreader, frame *Frame, pass uint32, 
 	return pg, nil
 }
 
-func (g PassGroup) invertVarDCT(frameBuffer [][][]float32, prev *PassGroup) error {
+func (g *PassGroup) invertVarDCT(frameBuffer [][][]float32, prev *PassGroup) error {
 	header := g.frame.Header
 	//zero := util.Point{}
 	if prev != nil {
@@ -92,15 +92,69 @@ func (g PassGroup) invertVarDCT(frameBuffer [][][]float32, prev *PassGroup) erro
 			}
 			//var foeff0 float32
 			//var foeff1 float32
-			//lfs := make([]float32, 2)
+			lfs := make([]float32, 2)
+			var coeff0 float32
+			var coeff1 float32
 			switch tt.transformMethod {
 			case METHOD_DCT:
 				util.InverseDCT2D(coeffs[c], frameBuffer[c], ppg, ppf, tt.getPixelSize(), scratchBlock[0], scratchBlock[1], false)
 				break
 			case METHOD_DCT8_4:
-				panic("not implemented")
+				coeff0 = coeffs[c][ppg.Y][ppg.X]
+				coeff1 = coeffs[c][ppg.Y+1][ppg.X]
+				lfs[0] = coeff0 + coeff1
+				lfs[1] = coeff0 - coeff1
+				for x := int32(0); x < 2; x++ {
+					scratchBlock[0][0][0] = lfs[x]
+					for iy := int32(0); iy < 4; iy++ {
+						startX := int32(0)
+						if iy == 0 {
+							startX = 1
+						}
+						for ix := startX; ix < 8; ix++ {
+							scratchBlock[0][iy][ix] = coeffs[c][ppg.Y+x+iy*2][ppg.X+ix]
+						}
+					}
+					ppf2 := util.Point{
+						X: ppf.X,
+						Y: ppf.Y,
+					}
+					ppf2.X += x << 2
+					if err := util.InverseDCT2D(scratchBlock[0], frameBuffer[c], util.ZERO, ppf2,
+						util.Dimension{Height: 4, Width: 8}, scratchBlock[1], scratchBlock[2], true); err != nil {
+						return err
+					}
+				}
+				break
+
 			case METHOD_DCT4_8:
-				panic("not implemented")
+				coeff0 = coeffs[c][ppg.Y][ppg.X]
+				coeff1 = coeffs[c][ppg.Y+1][ppg.X]
+				lfs[0] = coeff0 + coeff1
+				lfs[1] = coeff0 - coeff1
+				for y := int32(0); y < 2; y++ {
+					scratchBlock[0][0][0] = lfs[y]
+					for iy := int32(0); iy < 4; iy++ {
+						startX := int32(0)
+						if iy == 0 {
+							startX = 1
+						}
+						for ix := startX; ix < 8; ix++ {
+							scratchBlock[0][iy][ix] = coeffs[c][ppg.Y+y+iy*2][ppg.X+ix]
+						}
+					}
+					ppf2 := util.Point{
+						X: ppf.X,
+						Y: ppf.Y,
+					}
+					ppf2.Y += y << 2
+					if err := util.InverseDCT2D(scratchBlock[0], frameBuffer[c], util.ZERO, ppf2,
+						util.Dimension{Height: 4, Width: 8}, scratchBlock[1], scratchBlock[2], true); err != nil {
+						return err
+					}
+				}
+				break
+
 			case METHOD_AFV:
 				panic("not implemented")
 			case METHOD_DCT2:
