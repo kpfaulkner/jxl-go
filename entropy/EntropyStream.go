6582db38cfe6f19c19f14032aa3988d4f52434f1
Ken Faulkner <Ken.Faulkner@gmail.com>
2025-02-10 12:44:44 +1100

    correct fix for alphabet size

--- entropy/EntropyStream.go
+++ entropy/EntropyStream.go
@@ -137,7 +137,7 @@ func NewEntropyStreamWithReader(reader *jxlio.Bitreader, numDists int, disallowL
 				if alphaSize, err := reader.ReadBits(uint32(n)); err != nil {
 					return nil, err
 				} else {
-					alphabetSizes[i] = 1 + int32(1<<alphaSize)
+					alphabetSizes[i] = 1 + int32(1<<n+alphaSize)
 				}
 			} else {
 				alphabetSizes[i] = 1
