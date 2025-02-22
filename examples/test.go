3a2f778232c6ec1ba17788f9de205c1020f7c406
Ken Faulkner <Ken.Faulkner@gmail.com>
2025-02-22 16:09:59 +1100

    jump between test cases

--- examples/test.go
+++ examples/test.go
@@ -16,6 +16,7 @@ func main() {
 
 	// church image exercises different code pathways compared to lenna.jxl
 	//file := `c:/temp/ken-0-4.jxl`
+	//file := `../testdata/patches.jxl`
 	file := `../testdata/alpha-triangles.jxl`
 	//file := `../testdata/unittest.jxl`
 
