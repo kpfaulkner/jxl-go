b2e11c2b340d43d5dde49d445c5b6f9600be624b
Ken Faulkner <Ken.Faulkner@gmail.com>
2025-11-08 07:34:55 +1100

    ModularChannel test started

---
+++ frame/ModularChannel_test.go
@@ -0,0 +1,201 @@
+package frame
+
+import (
+	"testing"
+
+	"github.com/kpfaulkner/jxl-go/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewModularChannelWithAllParams(t *testing.T) {
+	mc := NewModularChannelWithAllParams(5, 5, 1, 1, true)
+	assert.NotNil(t, mc)
+}
+
+func TestAllocate(t *testing.T) {
+	mc := NewModularChannelWithAllParams(5, 5, 1, 1, true)
+	assert.NotNil(t, mc)
+
+	mc.allocate()
+	assert.NotNil(t, mc.buffer)
+}
+
+func TestPrediction(t *testing.T) {
+
+	for _, tc := range []struct {
+		name           string
+		mc             *ModularChannel
+		x              int32
+		y              int32
+		k              int32
+		expectedResult int32
+		expectErr      bool
+	}{
+		{
+			name:           "success k=0",
+			mc:             NewModularChannelWithAllParams(5, 5, 1, 1, true),
+			x:              0,
+			y:              0,
+			k:              0,
+			expectedResult: 0,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=1",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              1,
+			expectedResult: 1,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=2",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              2,
+			expectedResult: 5,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=3",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              3,
+			expectedResult: 3,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=4",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              4,
+			expectedResult: 5,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=5",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              5,
+			expectedResult: 5,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=6",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              6,
+			expectedResult: 4,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=7",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              7,
+			expectedResult: 10,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=8",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              8,
+			expectedResult: 0,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=9",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              9,
+			expectedResult: 1,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=10",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              10,
+			expectedResult: 0,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=11",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              11,
+			expectedResult: 2,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=12",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              12,
+			expectedResult: 7,
+			expectErr:      false,
+		},
+		{
+			name:           "success k=13",
+			mc:             makeFakeModularChannel(),
+			x:              1,
+			y:              1,
+			k:              13,
+			expectedResult: 5,
+			expectErr:      false,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+
+			res, err := tc.mc.prediction(tc.x, tc.y, tc.k)
+			if err != nil && !tc.expectErr {
+				t.Errorf("got error when none was expected : %v", err)
+			}
+			if err == nil && tc.expectErr {
+				t.Errorf("expected error but got none")
+			}
+			if err != nil && tc.expectErr {
+				return
+			}
+
+			assert.Equal(t, tc.expectedResult, res, tc.name)
+		})
+	}
+}
+
+func makeFakeModularChannel() *ModularChannel {
+	mc := NewModularChannelWithAllParams(5, 5, 1, 1, true)
+	mc.allocate()
+
+	i := int32(0)
+	// populate with pattern.
+	for y := 0; y < 5; y++ {
+		for x := 0; x < 5; x++ {
+			mc.buffer[x][y] = i
+			i++
+		}
+	}
+
+	// not the right type of data... but just want to confirm consistency with unit tests
+	mc.pred = util.MakeMatrix2D[int32](5, 5)
+	for y := 0; y < 5; y++ {
+		for x := 0; x < 5; x++ {
+			mc.pred[x][y] = i
+			i++
+		}
+	}
+	return mc
+}
